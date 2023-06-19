328864cb85a72c3ed3839c105d78984da7f0c5a7
JingSZhuo <jing2001@live.co.uk>
2023-06-19 23:27:59 +0100

    added year and month logic

--- age calculator app/src/App.tsx
+++ age calculator app/src/App.tsx
@@ -4,20 +4,43 @@ import './App.scss';
 function App() {
 
     
-  const CheckDateRange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const CheckDateRange = (event: React.ChangeEvent<HTMLInputElement>): boolean => {
     const parsedValueFloat: number = parseFloat(event.target.value);
     const parsedValueInteger: number = parseInt(event.target.value);
 
     if (!Number.isInteger( parsedValueFloat) || parsedValueFloat !== parsedValueInteger ){
-      console.log('Not a whole number')
+      console.log('Not a whole number');
+      return false;
     }
     if (!((parsedValueInteger > 0) && (parsedValueFloat < 32))) {
-      console.log('out of bounds')
+      console.log('out of bounds');
+      return false;
     }
+    return true;
   }
 
-  const ThrowError = () => {
+  const CalculateAge = (): void => {
 
+    //user input 
+    const dateInput: HTMLInputElement | null = document.getElementById('date') as HTMLInputElement;
+    const monthInput: HTMLInputElement | null = document.getElementById('month') as HTMLInputElement;
+    const yearInput: HTMLInputElement | null = document.getElementById('year') as HTMLInputElement;
+  
+    const dateObject = new Date(); //new date object to get current day/month/year to calculate difference
+
+    const yearDifference = dateObject.getFullYear() - parseInt(yearInput.value);
+
+    if ( dateObject.getMonth() > parseInt(monthInput.value)){
+      const monthDifference = dateObject.getMonth() - parseInt(monthInput.value);
+      console.log(dateObject.getMonth() , monthInput.value )
+      console.log('Month: ', monthDifference);
+    } else {
+      const newYearDifference = yearDifference-1;
+      const monthDifference = 12 + dateObject.getMonth() - parseInt(monthInput.value);
+      console.log(dateObject.getMonth() , monthInput.value )
+      console.log('Month: ', monthDifference, newYearDifference);
+    }
+    
   }
 
   return (
@@ -32,12 +55,15 @@ function App() {
               </div>
               <div className='individual-date-input'>
                 <label htmlFor='date'>MONTH</label>
-                <input type={'number'} id='date' placeholder='MM'/>
+                <input type={'number'} id='month' placeholder='MM'/>
               </div>
               <div className='individual-date-input'>
                 <label htmlFor='date'>YEAR</label>
-                <input type={'number'} id='date' placeholder='YYYY'/>
+                <input type={'number'} id='year' placeholder='YYYY'/>
               </div>
+            </div>
+            <div>
+              <input type={'submit'} id='submit' onClick={CalculateAge}/>
             </div>
           </div>
         </div>
