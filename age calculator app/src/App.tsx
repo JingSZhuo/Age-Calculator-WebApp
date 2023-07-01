70318918389ae2c946d5ca403cef371ee2ab8192
JingSZhuo <jing2001@live.co.uk>
2023-07-01 22:15:04 +0100

    fixed logic for calculating age + added scss responsiveness

--- age calculator app/src/App.tsx
+++ age calculator app/src/App.tsx
@@ -1,13 +1,7 @@
 import { useState } from 'react';
 import './App.scss';
 
-function isLeapYear(year: number) {
-  // Leap years are divisible by 4, except for years divisible by 100
-  // However, years divisible by 400 are still considered leap years
-  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
-}
-
-function App() {
+function App(){
 
   //DD-MM-YYYY
   const [date, setDate] = useState('--');
@@ -22,50 +16,56 @@ function App() {
   const VerifyInput = (e: any): void => {
     e.preventDefault();
 
-    //Max Dates in a month
-    const datesInMonth: number[] = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-     //new date object to get current day/month/year to calculate difference
+    //new date object to get current day/month/year to calculate difference
     const currentDate = new Date();
     //fetching user input via getElementById
-    const dateInput: HTMLInputElement | null = (document.getElementById('date') as HTMLInputElement);
+    const dateInput: HTMLInputElement | null = document.getElementById('date') as HTMLInputElement;
     const monthInput: HTMLInputElement | null = document.getElementById('month') as HTMLInputElement;
     const yearInput: HTMLInputElement | null = document.getElementById('year') as HTMLInputElement;
+    //ParsedInt values
+    const dateInputParsed = parseInt(dateInput.value);
+    const monthInputParsed = parseInt(monthInput.value);
+    const yearInputParsed = parseInt(yearInput.value);
 
-    //Check leap year (February)
-    if ( isLeapYear( parseInt(yearInput.value) ) === true ) {
-      console.log(`${parseInt(yearInput.value)} is a leap year`);
-      datesInMonth[2] = 29;
-    }
-    else { console.log("Not a leap year"); }
+    //User input pass through date object for date validation checks (month-1 because 11 is dec, 10 is nov etc...0 is jan)
+    const dateCheck = new Date(yearInputParsed, monthInputParsed-1, dateInputParsed);
 
     //Check if values are blank
-    if (dateInput.value === "" || monthInput.value === "" || yearInput.value === ""){
-      console.log('Must fill in all blanks');
+    if (dateInput.value === ""){
+      setErrorDate('This following fields are required');
+    } else { setErrorDate('')}
+    if (monthInput.value === "" ){
+      setErrorMonth('This following fields are required');
+    } else { setErrorMonth('')}
+    if (yearInput.value === ""){
+      setErrorYear('This following fields are required');
+    } else { setErrorYear('')}
+
+    if ( (dateInput.value === "" || monthInput.value === "" || yearInput.value === "")) {
       setDate('--'); setMonth('--'); setYear('--'); 
-      setErrorDate('This following fields are required')
       return;
-    } 
+    }
+
     //Check if year is larger than current year
-    if (parseInt(yearInput.value) > currentDate.getFullYear()){
-      //console.log('Year is larger than current year, try again');
+    if (yearInputParsed > currentDate.getFullYear()){
       setErrorDate(''); setErrorMonth(''); setErrorYear('Must be in the past');
       return;
     }
     //Check if month is between 0 and 12
-    if (parseInt(monthInput.value) > 12 || parseInt(monthInput.value) < 1){
-      //console.log('Month is not between 1 and 12, try again');
+    if (monthInputParsed > 12 || monthInputParsed < 1){
       setErrorDate(''); setErrorMonth('Must be a valid month'); setErrorYear('');
       return;
     }
-    //Check if month is between 0 and 12
-    if (parseInt(dateInput.value) > 31 || parseInt(dateInput.value) < 1){
-      //console.log('Month is not between 1 and 12, try again');
+    //Check if date is between 1 and 31
+    if (dateInputParsed > 31 || dateInputParsed < 1){
       setErrorDate('Must be a valid day'); setErrorMonth(''); setErrorYear('');
       return;
     }
     //Check for appropriate date format i.e. 31/4 is not appropriate as there is 30 days in april
-    if ( parseInt(dateInput.value) > datesInMonth[parseInt(monthInput.value)] ){
-      //console.log('Date format not appropriate');
+    if (dateInputParsed > dateCheck.getDate()) {
+      console.log("invalid date as year", yearInputParsed, dateCheck.getFullYear() );
+      console.log("invalid date as year", monthInputParsed, dateCheck.getMonth() );
+      console.log("invalid date as year", dateInputParsed, dateCheck.getDate() );
       setErrorDate('Must be a valid date'); setErrorMonth(''); setErrorYear('');
     }
     else{
@@ -76,27 +76,23 @@ function App() {
 
   const CalculateAge = (bdate: number, bmonth: number, byear: number): void => {
 
-    const datesInMonth: number[] = [31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-    const currentDate = new Date(); //new date object to get current day/month/year to calculate difference
-    //console.log(bdate)
+    const currentDate = new Date(); //new date object to get current day/month/year
     const birthDate = new Date(byear, bmonth, bdate);
 
     let ageInYear: number = currentDate.getFullYear() - birthDate.getFullYear();
     let ageInMonth: number = (currentDate.getMonth()+1) - birthDate.getMonth();
     let ageInDate: number = currentDate.getDate() - birthDate.getDate();
 
-    if (ageInMonth < 0){  //If the birth month is larger than current month
+    if (ageInMonth <= 0){  //If the birth month is larger than current month
       ageInYear--;
       ageInMonth+=12;
     }
-    if (ageInDate < 0){   //If the birth date is larger than current date
+    if (ageInDate <= 0){   //If the birth date is larger than current date
       ageInMonth--;
-      ageInDate+= datesInMonth[bmonth+1]
+      ageInDate = currentDate.getDate() + new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate() - bdate; //Gets the last day of the previous month
     }
 
     setYear(`${ageInYear}`); setMonth(`${ageInMonth}`); setDate(`${ageInDate}`);
-    console.log(`You are ${ageInDate} days, ${ageInMonth} months, and ${ageInYear} years old`)
   }
 
   return (
@@ -120,9 +116,9 @@ function App() {
                 <input type={'number'} id='year' placeholder='YYYY'/>
                 <p className='error-message'>{errorYear}</p>
               </div>
-            </div>
-            <div>
-              <input type={'submit'} id='submit'/>
+              <div className='submit-div'>
+                <input type={'submit'} id='submit' value=''/>
+              </div>
             </div>
           </form>
           <div className='output-container'>
